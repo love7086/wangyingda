46fa5d9e697a2e115ff3f0eae7668b89083f9167
love7086 <wang.yingd@husky.neu.edu>
2017-08-31 15:07:14 -0400

    ----------Updated at 8.31.2017-------- 1.Add class GithubTest 2.Add trivial method in GithubTest

--- src/main/java/GithubTest.java
+++ src/main/java/GithubTest.java
@@ -5,4 +5,7 @@ public class GithubTest {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    private void trivialMethod(){
+        System.out.println("Nothing");
+    }
 }
